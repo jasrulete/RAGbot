88193ff867add249474757091cd9d506dabdd308
jer2x <rulete.jeric@gmail.com>
2026-06-13 15:50:01 +0800

    updated langchain error

--- rag_engine.py
+++ rag_engine.py
@@ -2,7 +2,7 @@ import os
 from typing import List, Tuple, Optional, Iterator
 
 import trafilatura
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
